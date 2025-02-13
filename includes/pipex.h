7d67f7bc0bf49a7a729588cb6d51abe4ad9e076a
MoriP <kohei.moriyama426@gmail.com>
2025-02-13 21:45:24 +0900

    pipex

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: kmoriyam <kmoriyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 23:43:52 by kmoriyam          #+#    #+#             */
-/*   Updated: 2025/02/08 23:07:48 by kmoriyam         ###   ########.fr       */
+/*   Updated: 2025/02/11 22:17:07 by kmoriyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,4 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include "../libft/libft.h"
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: kmoriyam <kmoriyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 23:44:29 by kmoriyam          #+#    #+#             */
-/*   Updated: 2025/02/09 00:16:09 by kmoriyam         ###   ########.fr       */
+/*   Updated: 2025/02/13 21:42:25 by kmoriyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,25 @@
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 42
 #endif
-int	main(int ac, char *av[])
+
+// void	throw_error(int	error_no)
+// {
+// 	if (error_no == 1)
+// 		exit(EXIT_FAILURE);
+// }
+
+// void	validate_arg(int ac, char *av[])
+// {
+// 	if (ac < 2 || 4 < ac)
+// 		throw_error(1);
+// 	if (access(av[1], R_OK) == -1)
+// 	{
+// 		perror("access");
+// 		exit(EXIT_FAILURE);
+// 	}
+// }
+
+int	main(int ac, char *av[], char *envp[])
 {
 	int		input_fd;
 	int		pipe_fds[2];
@@ -26,9 +44,12 @@ int	main(int ac, char *av[])
 	pid_t	pid;
 	char	*env[] = {"PATH=/bin:/usr/bin", "HOME=/home/kmoriyam", NULL};
 	char	*file_path;
+	ssize_t		byte_read;
+	char	buf[BUFFER_SIZE];
 
 	if (ac < 2)
 		return (0);
+	// validate_arg(ac, av);
 	input_fd = open(av[1], O_RDONLY);
 	if (input_fd < 0)
 		return (0);
@@ -38,8 +59,8 @@ int	main(int ac, char *av[])
 		perror("pipe");
 		exit(EXIT_FAILURE);
 	}
-	printf("input_fd = %d\n", input_fd);
-	printf("pipe(): fds[0] = %d, fds[1] = %d\n", pipe_fds[0], pipe_fds[1]);
+	// printf("input_fd = %d\n", input_fd);
+	// printf("pipe(): fds[0] = %d, fds[1] = %d\n", pipe_fds[0], pipe_fds[1]);
 	pid = fork();
 	if (pid == -1)
 	{
@@ -49,29 +70,77 @@ int	main(int ac, char *av[])
 	else if (pid == 0)
 	{
 		close(pipe_fds[1]);
+		// printf("child going..., pid = %d\n", pid);
+		// while (1)
+		// {
+		// 	byte_read = read(pipe_fds[0], buf, BUFFER_SIZE);
+		// 	if (byte_read == -1)
+		// 	{
+		// 		perror("read");
+		// 		exit(EXIT_FAILURE);
+		// 	}
+		// 	if (byte_read == 0)
+		// 		break ;
+		// 	if (write(STDOUT_FILENO, buf, byte_read) != byte_read)
+		// 	{
+		// 		perror("write");
+		// 		exit(EXIT_FAILURE);
+		// 	}
+		// }
+		// write(STDOUT_FILENO, "\n", 1);
+		// close(STDOUT_FILENO);
+		// exit(EXIT_SUCCESS);
 		if (dup2(pipe_fds[0], STDIN_FILENO) == -1)
 		{
 			perror("dup2");
 			exit(EXIT_FAILURE);
 		}
+		printf("pfd = %d\n", pipe_fds[0]);
 		close(pipe_fds[0]);
-		printf("child going...\n");
-		printf("child: pid = %d\n", pid);
-		execve("/bin/cat", av, env);
+		// printf("child going...\n");
+		// printf("child: pid = %d\n", pid);
+		execve("/bin/cat", av, envp);
 		perror("execve");
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		close(pipe_fds[0]);
-		printf("parent going..., wait child(pid = %d)\n--------------------\n", pid);
-		waitpid(pid, &status, 0);
-		printf("\nchild(pid = %d) finished!\n", pid);
-		if (WIFEXITED(status))
-			printf("exit status = %d\n", WEXITSTATUS(status));
+		// if (dup2(STDOUT_FILENO, pipe_fds[1]) == -1)
+		// {
+		// 	perror("dup2");
+		// 	exit(EXIT_FAILURE);
+		// }
+		// close(STDOUT_FILENO);
+		while (1)
+		{
+			byte_read = read(input_fd, buf, BUFFER_SIZE);
+			if (byte_read == -1)
+			{
+				perror("read");
+				exit(EXIT_FAILURE);
+			}
+			if (byte_read == 0)
+				break ;
+			if (write(pipe_fds[1], buf, byte_read) != byte_read)
+			{
+				perror("write");
+				break ;
+			}
+		}
+		// printf("parent going..., wait child(pid = %d)\n\n", pid);
+		// if (write(pipe_fds[1], av[1], ft_strlen(av[1])) != ft_strlen(av[1]))
+		// {
+		// 	perror("write");
+		// 	exit(EXIT_FAILURE);
+		// }
+		close(input_fd);
 		close(pipe_fds[1]);
+		waitpid(pid, &status, 0);
+		printf("\nback to parent, child process finished!\n", pid);
+		// if (WIFEXITED(status))
+		// 	printf("exit status = %d\n", WEXITSTATUS(status));
 		exit(EXIT_SUCCESS);
 	}
-
 	return (0);
 }
